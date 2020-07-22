b8cf305c916c7159840af8587f9296dcf86a5ad7
Beiweibudong <Beiweibudong@users.noreply.github.com>
2020-07-22 09:45:59 +0800

    Update router.py

--- tongxunlu/router.py
+++ tongxunlu/router.py
@@ -5,7 +5,7 @@ from models import session, User
 
 app = Flask(__name__)
 # 这里登陆的是root用户，要填上自己的密码，MySQL的默认端口是3306，填上之前创建的数据库名text1
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:377814789@localhost:3306/map_test?charset=utf8"'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:/map_test?charset=utf8"'
 # 设置这一项是每次请求结束后都会自动提交数据库中的变动
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
